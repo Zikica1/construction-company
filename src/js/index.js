c5a89edc35c65d13b4b2349f72d4035582ed51bb
Žikica Drača <zikica.draca@gmail.com>
2023-01-11 18:39:47 +0100

    info section entry

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,23 @@ hamburgerBtn.addEventListener('click', () => {
   hamburgerBtn.classList.toggle('open')
   nav.classList.toggle('show-nav')
 })
+
+//observer info section
+
+const infoItems = document.querySelectorAll('.info-item');
+
+const optionInfo = {
+  threshold: 0.5,
+  rootMargin: "0px 0px -100px 0px"
+}
+
+const observerInfo = new IntersectionObserver((entries, observerInfo) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle('show-info', entry.isIntersecting)
+    if (entry.isIntersecting) observerInfo.unobserve(entry.target)
+  })
+}, optionInfo)
+
+infoItems.forEach((item) => {
+  observerInfo.observe(item)
+})
