77353702da5c3551a4a940ab376508dd222bcd91
Žikica Drača <zikica.draca@gmail.com>
2023-01-21 16:40:40 +0100

    contact section entry

--- src/js/index.js
+++ src/js/index.js
@@ -37,3 +37,41 @@ experienceItems.forEach((item) => {
 teamCard.forEach((item) => {
   observer.observe(item)
 })
+
+// observer contact section
+
+const map = document.querySelector('.contact-map')
+const cardContact = document.querySelectorAll('.contact-card')
+const colLeft = document.querySelectorAll('.col-left')
+
+const option2 = {
+  rootMargin: "0px 0px -50px 0px",
+  threshold: 0.5
+}
+
+const contactObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle('show-map', entry.isIntersecting)
+    if (entry.isIntersecting) contactObserver.unobserve(entry.target)
+  })
+})
+
+contactObserver.observe(map)
+
+
+
+
+const cardObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle('show-card', entry.isIntersecting)
+    if (entry.isIntersecting) cardObserver.unobserve(entry.target)
+  })
+}, option2)
+
+cardContact.forEach((item) => {
+  cardObserver.observe(item)
+})
+
+colLeft.forEach((item) => {
+  cardObserver.observe(item)
+})
